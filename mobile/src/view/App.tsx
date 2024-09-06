c3aca779f690bd4a5592ab9d85812563fb213c5b
wolver1ne10 <dimaspotapov0606@gmail.com>
2024-09-06 15:24:59 +0200

    ad location permission, start with theme

--- mobile/src/view/App.tsx
+++ mobile/src/view/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import {AppNavigator} from './navigation/navigators/App';
 import {View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ThemeProvider} from '@react-navigation/native';
+import {createTheme} from '@rneui/themed';
+
+const theme = createTheme({
+  lightColors: {
+    primary: '#e7e7e8',
+  },
+  darkColors: {
+    primary: '#000',
+  },
+  mode: 'light',
+});
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- mobile/src/view/screens/discover/map/indes.tsx
+++ mobile/src/view/screens/discover/map/indes.tsx
@@ -1,17 +1,256 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {PERMISSIONS} from 'react-native-permissions';
 // import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
+  var region = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+  var mapStyle = [
+    {
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#212121',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.icon',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          color: '#212121',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative.country',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#9e9e9e',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#bdbdbd',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.attraction',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.business',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#181818',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#616161',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          color: '#1b1b1b',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.place_of_worship',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.sports_complex',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.fill',
+      stylers: [
+        {
+          color: '#2c2c2c',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#8a8a8a',
+        },
+      ],
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#373737',
+        },
+      ],
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#3c3c3c',
+        },
+      ],
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#4e4e4e',
+        },
+      ],
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#616161',
+        },
+      ],
+    },
+    {
+      featureType: 'transit',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#757575',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#000000',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#3d3d3d',
+        },
+      ],
+    },
+  ];
+  useEffect(() => {
+    PERMISSIONS.IOS.LOCATION_WHEN_IN_USE;
+  }, []);
   return (
     <View style={{flex: 1}}>
       <MapView
+        initialRegion={region}
+        customMapStyle={mapStyle}
         mapType="standard"
         showsUserLocation
         showsMyLocationButton
         style={StyleSheet.absoluteFillObject}
-        provider="google"></MapView>
+        provider={PROVIDER_GOOGLE}></MapView>
     </View>
   );
 };
+//https://maps.googleapis.com/maps/api/js?key=AIzaSyALm683qL_pcgl96yrjTAiq-kVAbQaUjTE&loading=async&libraries=places&callback=initMap
