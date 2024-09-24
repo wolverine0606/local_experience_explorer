217d6c1e03f17e0f08f8dacce74606f77e7bb9f4
wolver1ne10 <dimaspotapov0606@gmail.com>
2024-09-24 22:11:44 +0200

    firebase native auth integration

--- mobile/ios/mobile/AppDelegate.mm
+++ mobile/ios/mobile/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 #import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,8 +8,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
   [GMSServices provideAPIKey:@"AIzaSyALm683qL_pcgl96yrjTAiq-kVAbQaUjTE"];
   self.moduleName = @"mobile";
+  [FIRApp configure];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
